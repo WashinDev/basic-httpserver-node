912ac0d6e819ac71022b34e13bcab497ae1317c6
Washington Neves <129608039+WashinDev@users.noreply.github.com>
2024-01-16 15:46:21 -0300

    aa

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,18 +1,24 @@
 const exec = require("child_process").exec;
 
 function start() {
-    console.log("Request handler 'start' was called.");
-    const content = "empty"
+  console.log("Request handler 'start' was called.");
 
-    exec("ls -lah", function (error, stdout, stderr) {
-    content = stdout;
-    });
-    return content;
+  exec(
+    "find /",
+    { timeout: 10000, maxBuffer: 20000 * 1024 },
+    function (error, stdout, stderr) {
+      response.writeHead(200, { "Content-Type": "text/plain" });
+      response.write(stdout);
+      response.end();
+    }
+  );
 }
 
 function upload() {
-    console.log("Request handler 'upload' was called.");
-    return "Hello Upload";
+  console.log("Request handler 'upload' was called.");
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.write("Hello Upload");
+  response.end();
 }
 
 exports.start = start;
